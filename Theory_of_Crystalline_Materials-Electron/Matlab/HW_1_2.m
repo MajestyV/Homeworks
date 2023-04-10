8133c9d2b5b9c30c1c6a28ced4723887f7b8356c
MajestyV <53797732+MajestyV@users.noreply.github.com>
2023-04-10 11:18:27 +0800

    Create HW_1_2.m

---
+++ Theory_of_Crystalline_Materials-Electron/Matlab/HW_1_2.m
@@ -0,0 +1,10 @@
+a = [13.0 1.96 0.44 0.12];
+%E = (3*a*a)/(a+a)+2*sqrt((a+a)/pi);
+H
+
+E1 = (3*a1*a1)/(a1+a1)+2*sqrt((a1+a1)/pi);
+E2 = (3*a2*a2)/(a2+a2)+2*sqrt((a2+a2)/pi);
+E3 = (3*a3*a3)/(a3+a3)+2*sqrt((a3+a3)/pi);
+E4 = (3*a4*a4)/(a4+a4)+2*sqrt((a4+a4)/pi);
+E = E1+E2+E3+E4;
+display(E);
